6cd4cbe06773e5093a9794cd52eb8c1f8ad1d8a4
刘建鑫 <seefly@vip.qq.com>
2018-09-03 15:08:41 +0800

    redisTemplate 坐下，好好听讲！！

---
+++ redis/src/main/java/org/seefly/redis/config/CacheConfig.java
@@ -0,0 +1,13 @@
+package org.seefly.redis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liujianxin
+ * @date 2018-09-03 15:03
+ */
+@Configuration
+public class CacheConfig {
+
+
+}
