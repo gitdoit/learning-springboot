cee609c8f4745a3381712bb1d947f742c47e6092
刘建鑫 <liujianxin@worken.cn>
2018-11-22 14:07:41 +0800

    java基础-future

--- java/src/main/java/org/seefly/juc/future/FutureData.java
+++ java/src/main/java/org/seefly/juc/future/FutureData.java
@@ -9,16 +9,19 @@ public class FutureData implements Data{
     protected RealData realData = null;
 
     public synchronized void setRealData(RealData realData){
+        // 防止重复设置
         if(isReady){
             return;
         }
         this.realData = realData;
         isReady = true;
+        // 响应完成，唤醒等待线程
         notifyAll();
     }
 
     @Override
     public synchronized String getResult(){
+        // 服务器还没有响应的时候，进入锁池等待
         while (!isReady){
             try {
                 wait();
