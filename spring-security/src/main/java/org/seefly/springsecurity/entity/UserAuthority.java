62e9e78934d40ff20994b0f141f95832743b9676
liujianxin <seefly@vip.qq.com>
2018-12-19 10:05:36 +0800

    spring security代码合并

---
+++ spring-security/src/main/java/org/seefly/springsecurity/entity/UserAuthority.java
@@ -0,0 +1,17 @@
+package org.seefly.springsecurity.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserAuthority {
+
+  /** null **/
+  private String username;
+  /** null **/
+  private String authority;
+
+}
