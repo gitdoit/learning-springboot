9c7683e0d0861d91b1ec7494bf2f11799419d74f
gitdoit <seefly@vip.qq.com>
2019-09-04 21:53:59 +0800

    移除java基础模块

--- java/src/main/java/org/seefly/juc/future/Data.java
+++
@@ -1,9 +0,0 @@
-package org.seefly.juc.future;
-
-/**
- * @author liujianxin
- * @date 2018-11-22 11:40
- */
-public interface Data {
-    String getResult();
-}
--- java/src/main/java/org/seefly/proxy/DemoInterface.java
+++
@@ -1,9 +0,0 @@
-package org.seefly.proxy;
-
-/**
- * @author liujianxin
- * @date 2018-12-06 11:30
- */
-public interface DemoInterface {
-    void sayHello();
-}
